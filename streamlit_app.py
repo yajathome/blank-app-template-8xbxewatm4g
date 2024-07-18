e159ea0dae68aa7a743f89b67cef724bfd395d87
yajathome <119949745+yajathome@users.noreply.github.com>
2024-07-18 16:40:33 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,7 +10,7 @@ e = st.sidebar.button('Back to Home Screen')
 st.markdown("""
     <style>
         section[data-testid="main.st-emotion-cache-bm2z3a.ea3mdgi8"]
-        background-image: url("https://images.unsplash.com/photo-1542281286-9e0a16bb7366");
+        background-image: url("https://www.bing.com/images/search?q=abstract%20picture%20of%20leaves&FORM=IQFRBA&id=985EFB983F7829BFBBE4334644F54AC54688E9A4");
         background-size: cover;
     </style>
 """,unsafe_allow_html=True)
