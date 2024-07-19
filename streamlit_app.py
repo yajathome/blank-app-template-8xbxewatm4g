912cb2b0ef66bf59f3ebcadb419ca35f0f271ee6
yajathome <119949745+yajathome@users.noreply.github.com>
2024-07-19 20:27:00 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,7 +12,7 @@ st.markdown("""
                 .st-emotion-cache-6qob1r {
                         #background: rgb(131,58,180);
                         #background: linear-gradient(0deg, rgba(131,58,180,1) 0%, rgba(253,29,29,1) 50%, rgba(252,176,69,1) 100%);
-                        background-color:#72b4ff;
+                        background-color:#55e898;
                         }
                  
                 .st-emotion-cache-bm2z3a {
