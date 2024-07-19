ef0745fa2bd5a210e98ec4de33382bce9e7c134d
yajathome <119949745+yajathome@users.noreply.github.com>
2024-07-19 14:25:27 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -39,6 +39,7 @@ st.markdown("""
                     align-items: center;
                     background: rgb(2,0,36);
                     background: linear-gradient(90deg, rgba(2,0,36,1) 0%, rgba(9,9,121,1) 35%, rgba(0,212,255,1) 100%);
+                    }
         </style> 
         """,
         unsafe_allow_html=True
