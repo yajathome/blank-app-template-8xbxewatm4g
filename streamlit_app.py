28dc447a8b2aecfae784e60473bca9be7291e1a7
yajathome <119949745+yajathome@users.noreply.github.com>
2024-07-20 18:03:32 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -66,6 +66,17 @@ st.markdown("""
                     border-radius: 20px;
                     padding:20px;
                 {
+                .st-emotion-cache-12fmjuu {
+                    position: fixed;
+                    top: 0px;
+                    left: 0px;
+                    right: 0px;
+                    height: 3.75rem;
+                    background: rgb(255, 255, 255);
+                    outline: none;
+                    z-index: 999990;
+                    display: block;
+                    background-color:rgba(0,0,0);
     """,unsafe_allow_html=True)
 
 def home():
