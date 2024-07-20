2447c942ca1f7bc657c87a13f4d2fd7a0e1f0f57
yajathome <119949745+yajathome@users.noreply.github.com>
2024-07-20 10:49:34 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,8 +10,8 @@ e = st.sidebar.button('Back to Home Screen')
 st.markdown("""
         <style>
                 .st-emotion-cache-6qob1r {
-                        #background: rgb(131,58,180);
-                        #background: linear-gradient(0deg, rgba(131,58,180,1) 0%, rgba(253,29,29,1) 50%, rgba(252,176,69,1) 100%);
+                        background: rgb(131,58,180);
+                        background: linear-gradient(0deg, rgba(131,58,180,1) 0%, rgba(253,29,29,1) 50%, rgba(252,176,69,1) 100%);
                         background-color:#55e898;
                         }
                  
