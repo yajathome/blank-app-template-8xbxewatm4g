cced13756010966a6c4f200a2f17ca12d8d9da55
yajathome <119949745+yajathome@users.noreply.github.com>
2024-07-26 12:11:32 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,7 +27,7 @@ st.markdown("""
                     -webkit-box-align: stretch;
                     align-items: center;
                     inset: 0px;
-                    background-image:url("https://static.vecteezy.com/system/resources/previews/001/228/271/non_2x/abstract-line-leaves-print-seamless-pattern-vector.jpg");
+                    background-image:url("https://static.vecteezy.com/system/resources/previews/011/189/962/original/abstract-wall-decor-design-with-hand-drawn-leaf-pattern-aesthetic-background-free-vector.jpg");
                     #
                     
                     
